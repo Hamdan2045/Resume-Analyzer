fde34b182458eea1a4cfdb92b09acbdc23f6abc9
Mohamed Hamdan <mohamedhamdan033@gmail.com>
2025-08-23 18:27:49 +0530

    Update SignupPage.jsx

--- frontend/src/SignupPage.jsx
+++ frontend/src/SignupPage.jsx
@@ -9,6 +9,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./SignupPage.css";
 import { withMinDelay } from "./withMinDelay";
+import { Link } from "react-router-dom";
 
 const API = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
 
